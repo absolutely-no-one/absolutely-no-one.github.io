e35d72701ed7594f5d2d062c33bf7c003db93acc
absolutely-no-one <autumn20071@icloud.com>
2021-03-27 21:14:26 -0400

    Added link to projects

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function jumpTo(element) {
     Jump(element);
 }
+
+function redirectTo(uuid) {
+    window.open('https://awesome-e.github.io/hs-tools/play-project/?id=' + uuid); // Via Awesome_E's play project tool
+}
