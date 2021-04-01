4928399694fa5fd0116d1914036934d2f2f6c881
absolutely-no-one <autumn20071@icloud.com>
2021-03-31 21:05:15 -0400

    Started question support

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function jumpTo(element) {
 function redirectTo(uuid) {
     window.open('https://awesome-e.github.io/hs-tools/play-project/?id=' + uuid); // Via Awesome_E's play project tool
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key == 'f') {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    }
+});
+/*
+$.getJSON("questions.json", function(json) {
+    let questions = json;
+    for (i=0; i<questions.length; i++) {
+        var element = document.getElementById('questions');
+        var question = document.createElement('p');
+        var answer = document.createElement('p');
+        question.setAttribute('class', 'question');
+        question.appendChild(document.createTextNode(questions[i].question));
+        answer.appendChild(document.createTextNode(questions[i].answer));
+        question.style.textTransform = 'capitalize';
+        element.appendChild(question);
+        element.appendChild(answer);
+    }
+});
+*/
+
+// above will be fully implemented later
