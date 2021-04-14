ab340a93900bb144a104ed42aca65573aea870a9
absolutely-no-one <autumn20071@icloud.com>
2021-04-13 20:59:29 -0400

    Added projects

--- index.js
+++ index.js
@@ -44,10 +44,25 @@ $.getJSON("questions.json", function(json) {
     }
 });
 
-$.getJSON('https://c.gethopscotch.com/api/v2/users/3944888/published_projects?api_token=4f7769439adf7ef8e482d2daef77375cd6d0158b65fcdca543b74b5c0c92&user%5Bauth_token%5D=qKok4lCx8YB9Wgp%2Bo862AA%3D%3D&user%5Bid%5D=4753230', function(json) {
-    let project = json;
-    for (i=0; i<project.projects.length; i++) {
-        console.log(project.projects.uuid);
+$.getJSON('https://api.allorigins.win/raw?url=' + encodeURIComponent('https://c.gethopscotch.com/api/v2/users/3944888/published_projects?api_token=4f7769439adf7ef8e482d2daef77375cd6d0158b65fcdca543b74b5c0c92&user%5Bauth_token%5D=qKok4lCx8YB9Wgp%2Bo862AA%3D%3D&user%5Bid%5D=4753230'), function(json) {
+    let user = json;
+    for (i=0; i<10; i++) {
+        var element = document.getElementById('recent-projects');
+        var project = document.createElement('div');
+        var image = document.createElement('IMG');
+
+        project.setAttribute('id', 'project' + i);
+        project.setAttribute('class', 'title is-4 project-title mui-panel has-text-centered');
+        project.appendChild(document.createTextNode(user.projects[i].title));
+
+        image.setAttribute('src', user.projects[i]['screenshot_url']);
+        image.setAttribute('width', 250);
+        image.setAttribute('height', 'auto');
+        image.setAttribute('id', 'image' + i);
+
+        element.appendChild(project);
+        document.getElementById('project' + i).appendChild(image);
+        document.getElementById('image' + i).addEventListener('click', function() { redirectTo(user.projects[i].uuid); });
     }
 })
 
