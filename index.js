9ada51f1bb503b98e3904b1eead3a0599b7ee2ae
absolutely-no-one <autumn20071@icloud.com>
2021-04-17 21:02:53 -0400

    Did stuff

--- index.js
+++ index.js
@@ -46,23 +46,37 @@ $.getJSON("questions.json", function(json) {
 
 $.getJSON('https://api.allorigins.win/raw?url=' + encodeURIComponent('https://c.gethopscotch.com/api/v2/users/3944888/published_projects?api_token=4f7769439adf7ef8e482d2daef77375cd6d0158b65fcdca543b74b5c0c92&user%5Bauth_token%5D=qKok4lCx8YB9Wgp%2Bo862AA%3D%3D&user%5Bid%5D=4753230'), function(json) {
     let user = json;
+    let projectUUIDS = []
     for (i=0; i<10; i++) {
+        projectUUIDS.push(user.projects[i].uuid);
         var element = document.getElementById('recent-projects');
         var project = document.createElement('div');
-        var image = document.createElement('IMG');
+        var title = document.createElement('div');
+        var thumbnail = document.createElement('div');
+        var stats = document.createElement('p');
 
         project.setAttribute('id', 'project' + i);
-        project.setAttribute('class', 'title is-4 project-title mui-panel has-text-centered');
-        project.appendChild(document.createTextNode(user.projects[i].title));
+        project.setAttribute('class', 'project-title mui-panel has-text-centered');
 
-        image.setAttribute('src', user.projects[i]['screenshot_url']);
-        image.setAttribute('width', 250);
-        image.setAttribute('height', 'auto');
-        image.setAttribute('id', 'image' + i);
+        title.setAttribute('class', 'title is-4');
+        title.appendChild(document.createTextNode(user.projects[i].title));
+
+        thumbnail.setAttribute('id', 'thumbnail' + i);
+        thumbnail.setAttribute('class', 'thumbnail');
+
+        stats.setAttribute('class', 'text is-6');
+        stats.appendChild(document.createTextNode(user.projects[i]['number_of_stars'] + ' likes, ' + user.projects[i].plants + ' plants'));
 
         element.appendChild(project);
-        document.getElementById('project' + i).appendChild(image);
-        document.getElementById('image' + i).addEventListener('click', function() { redirectTo(user.projects[i].uuid); });
+        document.getElementById('project' + i).appendChild(thumbnail);
+        // document.getElementById('project' + i).appendChild(title);
+        // document.getElementById('project' + i).appendChild(stats);
+        thumbnail.style.backgroundImage = 'url(' + user.projects[i]['screenshot_url'] + ')';
+        document.getElementById('thumbnail' + i).addEventListener('click', function(e) { 
+            var selectedChild = [].slice.call(document.getElementById('recent-projects').querySelectorAll('.thumbnail'), 0);
+            var n = selectedChild.indexOf(e.target);
+            redirectTo(user.projects[n].uuid); 
+        });
     }
 })
 
